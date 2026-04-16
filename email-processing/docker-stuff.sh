f37f4aa57ae44522bb2d3ffb001987d7387c7be9
scallighan <scallighan@scallighan.com>
2026-04-16 14:14:55 -0500

    adding mssql drivers

--- email-processing/docker-stuff.sh
+++ email-processing/docker-stuff.sh
@@ -3,4 +3,5 @@ docker build -t graph-tagger-agent .
 docker stop gta
 docker rm gta
 
-docker run -d --env-file .env -p 8888:80 --name gta graph-tagger-agent
+#docker run -d --env-file .env -p 8888:80 --name gta graph-tagger-agent
+docker run -d -p 8888:80 --name gta graph-tagger-agent
